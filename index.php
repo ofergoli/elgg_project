2aafc66d83dc9f38f8cd7ed24b1d07ebb8767a43
ofergoli <ofergolib@gmail.com>
2014-11-25 19:14:45 +0200

    index change

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 
 	//$pass = md5("ronkahat" . "M0ABlCEl");
 	//echo $pass;
-	echo "Hello";
-	export_csv("elgg_users_entity");
+	//echo "Hello";
+	export_csv("elgg_objects_entity");
 	//export_csv("elgg_access_collections");
 
 ?>
