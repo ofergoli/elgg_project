c973d969d95f4011288f0125db12be2f980745fd
ofergoli <ofergolib@gmail.com>
2014-11-30 22:20:05 +0200

    entrence and ui

--- index.php
+++ index.php
@@ -24,7 +24,6 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="css/elggstyle.css" type="text/css">
 </head>
 <body>
@@ -32,7 +31,7 @@
 				<img src="img/elgg_logo.png" alt="Elgg">
 	</header>
 	<div class='wapper'>
-		<div class="elgg-page-body">
+ 		<div class="elgg-page-body">
 			<h1>Execute CSV File choose table</h1>
 			<div class="download_section">
 				<form action="index.php" method="get">
@@ -51,9 +50,27 @@
 		<div class="elgg-page-body">
 			<h1>Create new Soical Network</h1>
 				<form action="index.php" method="get" value="create_sn">
-					<div>
-						<h3 >Make your own Soical Network : </h3>
-						<input type="submit" name="Create" value="Create" />
+					<h3 >Make your own Soical Network : </h3>
+					<div class="custom_div">
+						<div class="custom_div">
+							<h3 class="custom_ui">user name :</h3> <input type="text" value="" name="username" class="custom_ui"/>
+						</div>
+						<div class="custom_div">
+							<h3 class="custom_ui">password  :</h3> <input type="text" value="" name="password" class="custom_ui"/>
+						</div>
+						<div class="custom_div">
+							<h3 class="custom_ui">email     :</h3> <input type="text" value="" name="email" class="custom_ui"/>
+						</div>
+						<div class="custom_div">
+							<h3 class="custom_ui">display name:</h3> <input type="text" value="" name="displayname" class="custom_ui"/> 	
+						</div>
+						<div class="custom_div">
+							<h3 class="custom_ui">site name   :</h3> <input type="text" value="" name="sitename" class="custom_ui"/>
+						</div>
+						<div class="custom_submit">
+							 <input type="submit" name="Create" value="Create"  />
+						</div>
+					
 					</div>
 				</form>
 		</div>
