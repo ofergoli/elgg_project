182c4cae270ba59cbe12f07b9938c8d9c353ef4f
ofergoli <ofergolib@gmail.com>
2014-12-06 16:51:50 +0200

    icon

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <html>
   <head>
     <title>Social Netword</title>
+    <link rel="shortcut icon" href="img/icon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
