a37ed6ace2e3169ccf00e06d2399f40ce6649536
Lior Erez <lior-erez@users.noreply.github.com>
2015-05-25 14:32:56 +0300

    Revert "Lior branch"

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
 
-	$(".submenu > a").click(function (e) {
-		e.preventDefault();
-		var $li = $(this).parent("li");
-		var $ul = $(this).next("ul");
+  $(".submenu > a").click(function(e) {
+    e.preventDefault();
+    var $li = $(this).parent("li");
+    var $ul = $(this).next("ul");
 
-		if ($li.hasClass("open")) {
-			$ul.slideUp(350);
-			$li.removeClass("open");
-		} else {
-			$(".nav > li > ul").slideUp(350);
-			$(".nav > li").removeClass("open");
-			$ul.slideDown(350);
-			$li.addClass("open");
-		}
-	});
+    if($li.hasClass("open")) {
+      $ul.slideUp(350);
+      $li.removeClass("open");
+    } else {
+      $(".nav > li > ul").slideUp(350);
+      $(".nav > li").removeClass("open");
+      $ul.slideDown(350);
+      $li.addClass("open");
+    }
+  });
 
 
-	// $( "#search_text" ).autocomplete(
-	//   {
-	//      source:'ajaxsocial.php',
-	//      minLength: 1,
-	//      delay:0,
-	//      // select:function(){
-	//      //   $("#img1").attr('src',"images/flags/"  + $("#text").val() + ".png");
-	//      // }
-	//   });
-
+  // $( "#search_text" ).autocomplete(
+  //   {
+  //      source:'ajaxsocial.php',
+  //      minLength: 1,
+  //      delay:0,
+  //      // select:function(){
+  //      //   $("#img1").attr('src',"images/flags/"  + $("#text").val() + ".png");
+  //      // }
+  //   });
+  
 });
