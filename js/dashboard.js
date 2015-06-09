addca27618da729d4f907eb07a146d90240af164
lior-erez <lior.e85@gmail.com>
2015-06-09 21:39:21 +0300

    Implemented export database

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,7 @@
 	var csvDlBtn = $('#csv-download').hide();
 	var spinner = $('.fa-spinner').hide();
 	$('button#export-db').on('click', function (e) {
+		csvDlBtn.hide();
 		spinner.show();
 		$.post('backup_restore.php', {dbName: $('input#sn-key').val()}, function (data) {
 			var result = JSON.parse(data);
