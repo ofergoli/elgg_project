504abcde521b95d4f837aaf78f82cb0d5a00d419
lior-erez <lior.e85@gmail.com>
2015-06-17 17:45:39 +0300

    Import database UI

--- backup_restore.php
+++ backup_restore.php
@@ -5,12 +5,17 @@ if (isset($_GET['sn'])) {
 	$sn_key = $_GET['sn'];
 }
 if (isset($_POST['dbName'])) {
-	$path = DataQueries::GetAllTables($_POST['dbName']);
-	$result = new stdClass();
-	$result->filename = basename($path);
-	$result->url = $Url . "/" . create_zip($path);
-	echo json_encode($result);
-	exit;
+	if (isset($_POST['zipFile'])) {
+		echo json_encode(55);
+		exit;
+	} else {
+		$path = DataQueries::GetAllTables($_POST['dbName']);
+		$result = new stdClass();
+		$result->filename = basename($path);
+		$result->url = $Url . "/" . create_zip($path);
+		echo json_encode($result);
+		exit;
+	}
 }
 
 function create_zip($path)
@@ -39,14 +44,14 @@ function create_zip($path)
 ?>
 
 <div class="row">
-	<div class="col-sm-5">
+	<div id="export-container" class="col-sm-5">
 		<legend>
 			Export Database
 		</legend>
 		<dl class="dl-horizontal">
 			<dt>Export database to CSV:</dt>
 			<dd>
-				<button id="export-db" class="btn btn-default">
+				<button id="export-db-btn" class="btn btn-default">
 					<i class="glyphicon glyphicon-export"></i>
 					Export to CSV
 				</button>
@@ -59,18 +64,34 @@ function create_zip($path)
 			</dd>
 		</dl>
 	</div>
-	<div class="col-sm-5">
+	<div id="import-container" class="col-sm-5">
 		<legend>
 			Import Database
 		</legend>
 		<dl class="dl-horizontal">
 			<dt>Import database from CSV:</dt>
 			<dd>
-				<button id="import-db" class="btn btn-default">
+				<button id="upload-csv-btn" class="btn btn-default">
+					Upload CSV files
+				</button>
+				<input id="upload-csv-file" name="zipFile" type="file" accept="application/zip"
+					   style="visibility: hidden"/>
+				<?php echo '<input id="sn-key" class="hidden_input" style="visibility: hidden;" value="' . $sn_key . '"/>'; ?>
+			</dd>
+			<dt></dt>
+			<dd>
+				<label id="override-data-cb">
+					<input type="checkbox"/>
+					Override existing data
+				</label>
+			</dd>
+			<dt></dt>
+			<dd>
+				<!-- <h3><i class="fa fa-spinner fa-pulse"></i></h3>-->
+				<a id="csv-import-btn" class="btn btn-default" href="">
 					<i class="glyphicon glyphicon-import"></i>
 					Import from CSV
-				</button>
-				<?php echo '<input id="sn-key" class="hidden_input" style="visibility: hidden;" value="' . $sn_key . '"/>'; ?>
+				</a>
 			</dd>
 		</dl>
 	</div>
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,47 @@
 (function () {
-	var csvDlBtn = $('#csv-download').hide();
-	var spinner = $('.fa-spinner').hide();
-	$('button#export-db').on('click', function (e) {
-		csvDlBtn.hide();
-		spinner.show();
-		$.post('backup_restore.php', {dbName: $('input#sn-key').val()}, function (data) {
+
+	var csvDownloadBtn = $('#csv-download').hide(),
+		csvImportBtn = $('#csv-import-btn').hide(),
+		overrideDataCb = $('#override-data-cb').hide(),
+		snKey = $('input#sn-key').val(),
+		inputFile = $('#upload-csv-file');
+
+	$('.fa-spinner').hide();
+
+	$('button#export-db-btn').on('click', function () {
+		csvDownloadBtn.hide();
+		var spinner = $('div#export-container').find('.fa-spinner').show();
+		$.post('backup_restore.php', {
+			dbName: snKey
+		}, function (data) {
 			var result = JSON.parse(data);
-			csvDlBtn.attr('href', result.url)
+			csvDownloadBtn.attr('href', result.url)
 				.attr('download', result.filename)
 				.show();
 			spinner.hide();
 		});
 	});
+
+	$('#upload-csv-btn').on('click', function () {
+		inputFile.trigger('click')
+
+			.change(function (e) {
+				if (this.files.length > 0) {
+					csvImportBtn.show();
+					overrideDataCb.show();
+					spinner.hide();
+				}
+			});
+	});
+
+	csvImportBtn.on('click', function () {
+		$.post('backup_restore.php', {
+			dbName: snKey,
+			zipFile: inputFile.files[0],
+			overrideData: overrideDataCb.children('input:checkbox').prop('checked')
+		}, function (data) {
+			console.log(JSON.stringify(data));
+		});
+	});
+
 })();
