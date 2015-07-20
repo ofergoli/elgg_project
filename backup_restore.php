542d00c32e2776a8e74c8a0636f161befbaa409d
Lior Erez <lior.e85@gmail.com>
2015-07-20 21:00:50 +0300

    Upload zip file to server

--- backup_restore.php
+++ backup_restore.php
@@ -4,9 +4,46 @@ include_once('utility.php');
 if (isset($_GET['sn'])) {
 	$sn_key = $_GET['sn'];
 }
+
+if ($_FILES["zip_file"]["name"]) {
+	$filename = $_FILES["zip_file"]["name"];
+	$source = $_FILES["zip_file"]["tmp_name"];
+	$type = $_FILES["zip_file"]["type"];
+
+	$name = explode(".", $filename);
+	$accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
+	foreach ($accepted_types as $mime_type) {
+		if ($mime_type == $type) {
+			$okay = true;
+			break;
+		}
+	}
+
+	$continue = strtolower($name[1]) == 'zip' ? true : false;
+	if (!$continue) {
+		$message = "The file you are trying to upload is not a .zip file. Please try again.";
+	}
+
+	$target_path = "C:/xampp/htdocs/elgg_project/tmp/upload/" . $filename;  // change this to the correct site path
+	if (move_uploaded_file($source, $target_path)) {
+		$zip = new ZipArchive();
+		$x = $zip->open($target_path);
+		if ($x === true) {
+			$zip->extractTo("C:/xampp/htdocs/elgg_project/tmp/upload/" . substr($filename, 0, strlen($filename) - 4)); // change this to the correct site path
+			$zip->close();
+
+			unlink($target_path);
+		}
+		$message = "Your .zip file was uploaded and unpacked.";
+	} else {
+		$message = "There was a problem with the upload. Please try again.";
+	}
+}
+
+
 if (isset($_POST['dbName'])) {
 	if (isset($_POST['zipFile'])) {
-		echo json_encode(55);
+		echo json_encode($_POST['zipFile']);
 		exit;
 	} else {
 		$path = DataQueries::GetAllTables($_POST['dbName']);
@@ -41,6 +78,11 @@ function create_zip($path)
 	return $path . ".zip";
 }
 
+function extract_zip($zipFile)
+{
+
+}
+
 ?>
 
 <div class="row">
@@ -65,35 +107,50 @@ function create_zip($path)
 		</dl>
 	</div>
 	<div id="import-container" class="col-sm-5">
+
+
+
+<!--		<form enctype="multipart/form-data" method="post" action="backup_restore.php">-->
+<!--			<label>Choose a zip file to upload: <input type="file" name="zip_file"/></label>-->
+<!--			<br/>-->
+<!--			<input type="submit" name="submit" value="Upload"/>-->
+<!--		</form>-->
+
+
+
 		<legend>
 			Import Database
 		</legend>
-		<dl class="dl-horizontal">
-			<dt>Import database from CSV:</dt>
-			<dd>
-				<button id="upload-csv-btn" class="btn btn-default">
-					Upload CSV files
-				</button>
-				<input id="upload-csv-file" name="zipFile" type="file" accept="application/zip"
-					   style="visibility: hidden"/>
-				<?php echo '<input id="sn-key" class="hidden_input" style="visibility: hidden;" value="' . $sn_key . '"/>'; ?>
-			</dd>
-			<dt></dt>
-			<dd>
-				<label id="override-data-cb">
-					<input type="checkbox"/>
-					Override existing data
-				</label>
-			</dd>
-			<dt></dt>
-			<dd>
-				<!-- <h3><i class="fa fa-spinner fa-pulse"></i></h3>-->
-				<a id="csv-import-btn" class="btn btn-default" href="">
-					<i class="glyphicon glyphicon-import"></i>
-					Import from CSV
-				</a>
-			</dd>
-		</dl>
+		<form id="upload-zip" enctype="multipart/form-data" method="post" action="backup_restore.php">
+
+			<dl class="dl-horizontal">
+				<dt>Import database from CSV:</dt>
+				<dd>
+					<button type="button" id="upload-csv-btn" class="btn btn-default">
+						Upload CSV files
+					</button>
+
+					<input id="upload-csv-file" name="zip_file" type="file" accept="application/zip"
+						   style="visibility: hidden"/>
+					<?php echo '<input id="sn-key" class="hidden_input" style="visibility: hidden;" value="' . $sn_key . '"/>'; ?>
+				</dd>
+				<dt></dt>
+				<dd>
+					<label id="override-data-cb">
+						<input name="override_data" type="checkbox"/>
+						Override existing data
+					</label>
+				</dd>
+				<dt></dt>
+				<dd>
+					<!-- <h3><i class="fa fa-spinner fa-pulse"></i></h3>-->
+					<button id="csv-import-btn" class="btn btn-default" href="">
+						<i class="glyphicon glyphicon-import"></i>
+						Import from CSV
+					</button>
+				</dd>
+			</dl>
+		</form>
 	</div>
 	<div class="col-sm-2">
 		<img id="db-img" class="pull-right" src="img/db.png" alt="Database"/>
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,47 +1,36 @@
 (function () {
 
-	var csvDownloadBtn = $('#csv-download').hide(),
-		csvImportBtn = $('#csv-import-btn').hide(),
-		overrideDataCb = $('#override-data-cb').hide(),
-		snKey = $('input#sn-key').val(),
-		inputFile = $('#upload-csv-file');
+    var csvDownloadBtn = $('#csv-download').hide(),
+        csvImportBtn = $('#csv-import-btn').hide(),
+        overrideDataCb = $('#override-data-cb').hide(),
+        snKey = $('input#sn-key').val(),
+        inputFile = $('#upload-csv-file');
 
-	$('.fa-spinner').hide();
+    $('.fa-spinner').hide();
 
-	$('button#export-db-btn').on('click', function () {
-		csvDownloadBtn.hide();
-		var spinner = $('div#export-container').find('.fa-spinner').show();
-		$.post('backup_restore.php', {
-			dbName: snKey
-		}, function (data) {
-			var result = JSON.parse(data);
-			csvDownloadBtn.attr('href', result.url)
-				.attr('download', result.filename)
-				.show();
-			spinner.hide();
-		});
-	});
+    $('button#export-db-btn').on('click', function () {
+        csvDownloadBtn.hide();
+        var spinner = $('div#export-container').find('.fa-spinner').show();
+        $.post('backup_restore.php', {
+            dbName: snKey
+        }, function (data) {
+            var result = JSON.parse(data);
+            csvDownloadBtn.attr('href', result.url)
+                .attr('download', result.filename)
+                .show();
+            spinner.hide();
+        });
+    });
 
-	$('#upload-csv-btn').on('click', function () {
-		inputFile.trigger('click')
+    $('#upload-csv-btn').on('click', function () {
+        inputFile.trigger('click')
 
-			.change(function (e) {
-				if (this.files.length > 0) {
-					csvImportBtn.show();
-					overrideDataCb.show();
-					spinner.hide();
-				}
-			});
-	});
-
-	csvImportBtn.on('click', function () {
-		$.post('backup_restore.php', {
-			dbName: snKey,
-			zipFile: inputFile.files[0],
-			overrideData: overrideDataCb.children('input:checkbox').prop('checked')
-		}, function (data) {
-			console.log(JSON.stringify(data));
-		});
-	});
+            .change(function (e) {
+                if (this.files.length > 0) {
+                    csvImportBtn.show();
+                    overrideDataCb.show();
+                }
+            });
+    });
 
 })();
